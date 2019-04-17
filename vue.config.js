fea27f3d478d8155f65a397615c4ac137f7a9dac
XSJXSJ <970369982@qq.com>
2019-04-17 16:59:41 +0800

    js打包压缩

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const productionGzipExtensions = ['js', 'css'];
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
+    configureWebpack: config => {
+        if (isProduction) {
+            config.plugins.push(new CompressionWebpackPlugin({
+                algorithm: 'gzip',
+                test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+                threshold: 10240,
+                minRatio: 0.8
+            }))
+        }
+    },
+
     devServer: {
         port: 8081,     // 端口
     },
